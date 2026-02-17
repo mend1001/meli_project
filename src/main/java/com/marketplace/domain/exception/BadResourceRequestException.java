8ee46603e1be70a7638e63d24b380bf4d43ffcd6
mend1001 <mend1001mend1001@gmail.com>
2026-02-16 21:37:28 -0500

    agregar BadResourceRequestException para manejar solicitudes inválidas

--- src/main/java/com/marketplace/domain/exception/BadResourceRequestException.java
+++ src/main/java/com/marketplace/domain/exception/BadResourceRequestException.java
@@ -1,11 +1,6 @@
-package com.hackerrank.sample.exception;
+package com.marketplace.domain.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BadResourceRequestException extends RuntimeException {
-    public BadResourceRequestException(String msg) {
-        super(msg);
-    }
+    public BadResourceRequestException(String message) { super(message); }
 }
+
